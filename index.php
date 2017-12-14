8dd3422b33b30138682ad853ceae0e9ec6aab84c
soleynenero <soleyne.nero@etu.u-pec.fr>
2017-12-14 14:16:44 +0100

    add connexion to database

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-	echo 'HELLO MOIJV';
+	require_once("connexion.php");
+
+	$resultat = $pdo->prepare("SELECT * FROM game");
+	$resultat->execute();
+	$game = $resultat->fetchAll(PDO::FETCH_ASSOC);
+
+	echo '<pre>';
+		print_r($game);
+	echo '</pre>';
+	
