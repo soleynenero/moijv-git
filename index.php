dcb8924c1df617573741d2a9a86c964957119e87
soleynenero <soleyne.nero@etu.u-pec.fr>
2017-12-14 17:00:44 +0100

    add new login form

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
               <a class="nav-link" href="#">Location</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Contact</a>
+				<a class="nav-link" href="login.php">Connexion</a>
             </li>
           </ul>
         </div>
---
+++ login.php
@@ -0,0 +1,102 @@
+<?php
+require_once("connexion.php");
+
+$resultat = $pdo->prepare("SELECT * FROM user");
+$resultat->execute();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title>Moi JV</title>
+
+<!-- Bootstrap core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+<link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Moi JV</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Accueil
+            <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">A propos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Location</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Connexion</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+
+  <!-- <div class="row"> -->
+
+    <h2>Connexion</h2>
+
+    <form action="#" method="post">
+
+    <div class="form-group">
+      <label for="username">username</label>
+      <input type="text" id="username" name="username">
+    </div>
+
+    <div class="form-group">
+      <label for="password">password</label>
+      <input type="text" id="password" name="password">
+    </div>
+
+        <input type="submit" value="Valider"><br><br>
+
+    </form>
+
+
+    <!-- </div> -->
+      <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Moi JV</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
